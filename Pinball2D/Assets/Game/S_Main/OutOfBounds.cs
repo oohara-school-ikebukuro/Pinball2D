ab12cf53c38dd0674fdf2b2b9e99ddfcbde43e4f
shasu <l7momip1z6junin3y@outlook.jp>
2025-09-05 10:04:50 +0900

    リポップ実装

--- Pinball2D/Assets/Game/S_Main/OutOfBounds.cs
+++ Pinball2D/Assets/Game/S_Main/OutOfBounds.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class OutOfBounds : MonoBehaviour
 {
+    [SerializeField] private Transform respawnPoint;
+
     private void Start()
     {
         // 当たり判定を行うんですが、物理判定は行わないよ
         var col = GetComponent<Collider2D>();
         col.isTrigger = true;
+
+        // リスポーン描画を消す
+        respawnPoint.gameObject.SetActive(false);
     }
 
-    // 当たり判定をしなくなった対象がいれば発火する。
+    // 当たり判定をしなくなった対象がいれば発火する
     private void OnTriggerExit2D(Collider2D collision)
     {
         if(!collision.CompareTag("Ball"))
         {
             return;
         }
+        // Debug.Log("ボールが、脱走しました");
 
-        Debug.Log("ボールが、脱走しました");
+        // ボールの位置を、リポップ位置に修正します
+        collision.transform.position = respawnPoint.position;
+
+        // RigidBodyを取得
+        var rb = collision.attachedRigidbody;
+        // 速度をリセットしますよ。
+        if (rb != null) rb.linearVelocity = Vector2.zero;
     }
 }
